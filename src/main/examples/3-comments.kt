ebb21db6a131af5b5dd0aa3b164a37b26bfb734a
Ashraf-Hamdoun <78766971+Ashraf-Hamdoun@users.noreply.github.com>
2022-08-29 03:29:36 -0700

    finish comments

---
+++ src/main/examples/3-comments.kt
@@ -0,0 +1,12 @@
+fun main() {
+    // this is a single line comment
+
+    print("Hello my comments")  // comment after the code
+
+    /*
+    * This is a
+    * multiple lines
+    * comment :)
+    * These comments will not be executed
+    */
+}
